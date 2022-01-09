c3000a791322769562867c673585803c7bcc7e39
marifrahmawan <m1600018181@webmail.uad.ac.id>
2022-01-09 10:29:11 +0800

    adding more components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,31 @@
+import styled from 'styled-components';
+import Intro from './components/Intro';
+import Navbar from './components/Navbar';
+
+const Container = styled.div`
+  height: 100vh;
+  overflow: hidden;
+`;
+
+const IntoShape = styled.div`
+  height: 100%;
+  width: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: -1;
+  clip-path: polygon(63% 0, 100% 0, 100% 100%, 45% 100%);
+  background-color: crimson;
+`;
+
 const App = () => {
-  return <div>Hello world!</div>;
+  return (
+    <Container>
+      <Navbar />
+      <Intro />
+      <IntoShape />
+    </Container>
+  );
 };
 
 export default App;
